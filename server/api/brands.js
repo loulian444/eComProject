9be694b10d1865c0942ff823758579dac7d000c2
Lisa Wu Zheng <lisawuzheng@lisas-imac.mynetworksettings.com>
2024-04-14 20:13:22 -0400

    stuff shows

--- server/api/brands.js
+++
@@ -1,7 +0,0 @@
-const router = require(`express`).Router();
-
-router.get(`/`, (req, res) => {
-  res.send(`welcome to brand router`);
-});
-
-module.exports = router;
--- server/api/categories.js
+++
@@ -1,7 +0,0 @@
-const router = require(`express`).Router();
-
-router.get(`/`, (req, res) => {
-  res.send(`welcome to category router`);
-});
-
-module.exports = router;
